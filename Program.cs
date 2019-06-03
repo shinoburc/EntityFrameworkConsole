9a7837f34f26284301646ab89ed471098a3fcb17
shinoburc <dandydot@gmail.com>
2019-06-03 17:38:59 +0900

    fix

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkConsole
